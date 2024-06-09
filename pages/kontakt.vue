35f547ef36de9e0488872ed4ce8125b232f7a6ed
shorty <ungegrieftfirma@gmail.com>
2024-06-09 18:45:36 +0200

    contact page frontend finished

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -1,12 +1,47 @@
 <script setup lang="ts">
 
+import WriteContactContainer from "~/components/contact/WriteContactContainer.vue";
+import ThumbnailComponent from "~/components/home/ThumbnailComponent.vue";
 </script>
 
 <template>
+  <Header></Header>
+  <ThumbnailComponent></ThumbnailComponent>
+  <main>
 
+    <div class="container">
 
+      <div class="contact-title">
+        <span>Kontakt</span>
+      </div>
+
+      <div class="contact-info">
+        <WriteContactContainer></WriteContactContainer>
+      </div>
+
+    </div>
+
+  </main>
+  <Footer></Footer>
 </template>
 
 <style scoped>
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.contact-title span {
+  text-transform: uppercase;
+  color: #1A1A1A;
+  font-size: 8rem;
+  font-family: "ZenDots", sans-serif;
+}
+
+.contact-info {
+  margin-top: -6rem;
+}
 
 </style>
